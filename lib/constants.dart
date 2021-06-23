c2c788a23d8658b2cfa95470be7a5d7fb3d83462
Jagat Mettra Chakma <jagatchakmajc@gmail.com>
2021-06-23 10:57:51 +0600

    3th commit single page added

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 
-const kPrimaryColors = Color(0xffff7943);
+const kPrimaryColors = Color(0xffb743ff);
 const kPrimaryLightColor = Color(0xffffecdf);
 const kPrimaryGradientColor = LinearGradient(
     begin: Alignment.topLeft,
     end: Alignment.bottomRight,
-    colors: [Color(0xffffa53e), Color(0xffff7643)]);
+    colors: [Color(0xffa77bec), Color(0xff9147bf)]);
 const kSecondaryColor = Color(0xff979797);
 const kTextColor = Color(0xff757575);
 const kAnimationDuration = Duration(milliseconds: 200);
